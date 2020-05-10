40dec663372cfd243fc3357950bb5d582b1f1f4f
Benji Spetter-Goldstein <benjisg@bu.edu>
2020-05-10 19:46:57 -0400

    moved settings data to new file settings.py

--- garprtotxt.py
+++ garprtotxt.py
@@ -2,116 +2,12 @@ import json
 import requests
 from datetime import datetime
 import time
+import settings
 
-#Player class
-class Player:
-    def __init__(self, id, name):
-        #id taken from GARPR player page
-        self.id = id
-        #name is name that will display in all output formats
-        self.name = name
-
-#specify maximum number of retries that can occur during request to the server    
-maxretries = 20
-
-#specify start and endtimes of season to collect data from (can be any timeframe)
-seasonstart = datetime(2019, 9, 23)
-seasonend = datetime(2020, 3, 15)
-
-#create all players
-AdmiralZhao = Player('58cd956a1d41c8259fa1fd0b', 'Admiral Zhao')
-Alt = Player('5c65f982421aa93db94e98a5', 'Alt')
-Arcade = Player('58cc88171d41c8259fa1fc21', 'Arcade')
-Artelind = Player('58d20b761d41c8259fa1ff7b', 'Artelind')
-Arty = Player('53c64c738ab65f6e27751221', 'Arty')
-Bank = Player('59ed7c721d41c84361b79498', 'Bank')
-Beansoup = Player('5e444f96421aa93e12ba4f9e', 'beansoup')
-BeastBoy = Player('5d6a8bb3421aa954d874ad46', 'BeastBoy')
-Ben = Player('5dbb8aca421aa97778f4f808', 'Ben')
-BigJoig = Player('545c83e88ab65f126349d8bb', 'BigJoig')
-BigRedAnimeBike = Player('58752d371d41c82e80d558a3', 'BigRedAnimeBike')
-Bonfire10 = Player('58c7303b1d41c8259fa1f8dd', 'bonfire10')
-BonkCushy = Player('542cf48e8ab65f5dcb8ce4ab', 'BonkCushy')
-Clutch = Player('5d698f0b421aa954d874acfb', 'Clutch')
-CNIU = Player('58cc8c0f1d41c8259fa1fc49', 'CNIU')
-Coach = Player('5c6ef669421aa93db94e9954', 'Coach')
-Dimension = Player('5b16ba781d41c86e982900e4', 'Dimension')
-DrewNG = Player('596272821d41c840fdcf8804', 'DrewNG')
-DrLobster = Player('5d4e3f57421aa954d874ac18', 'DrLobster')
-Dudutsai = Player('58c732681d41c8259fa1f905', 'dudutsai')
-Een = Player('59ed70bd1d41c84361b79428', 'Een')
-Elen = Player('5e444f96421aa93e12ba4f9d', 'Elen')
-Feo = Player('5d818416421aa954d874aed1', 'Feo')
-Ferox = Player('58cc6fc61d41c8259fa1fb6b', 'Ferox')
-FutureShock = Player('5ade76ef1d41c852c7240f91', 'Future Shock')
-Giraffe = Player('5d423a5a421aa954d874ab76', 'Giraffe')
-Glasper = Player('58d09a571d41c8259fa1fe86', 'Glasper')
-Glock = Player('5a4ba1961d41c84881946784', 'glock in my toyota')
-Golden = Player('58c7303b1d41c8259fa1f8d2', 'Golden')
-Guex = Player('58c73fce1d41c8259fa1fa3d', 'Guex')
-Guillotine = Player('5d295939421aa90f9fc1bd97', 'Guillotine')
-GWM420 = Player('5a4ba23b1d41c84881946788', 'GWM420')
-Hexjo = Player('5d6fe5d9421aa954d874adfb', 'Hexjo')
-Hysteric = Player('5b633156421aa91fa088af7a', 'Hysteric')
-IOA = Player('58c72fd21d41c8259fa1f8c9', 'IOA')
-JNaut = Player('58c7317e1d41c8259fa1f8e3', 'JNaut')
-Joyboy = Player('58c73c7e1d41c8259fa1f9c5', 'Joyboy')
-Kalvar = Player('58c726dd1d41c8259fa1f870', 'Kalvar')
-Kikoho = Player('5b40def21d41c8456a883413', 'Kikoho')
-Klaps = Player('58cc6fc61d41c8259fa1fb57', 'Klaps')
-Kota = Player('58cc6fc61d41c8259fa1fb6a', 'Kota')
-Lint = Player('545c8e5f8ab65f12c01061ca', 'lint')
-Lochist = Player('5c86c4d1421aa93db94e9ab6', 'Lochist')
-Louis = Player('5ba19af6421aa91fa088b641', 'Louis')
-Meep = Player('58c73d181d41c8259fa1f9d0', 'Meep')
-MrHeat = Player('59ed78aa1d41c84361b7947c', 'Mr. Heat')
-Nugget = Player('58c729f41d41c8259fa1f8a2', 'Nugget')
-Omar = Player('5d16d616421aa90f9fc1bc56', 'Omar')
-Palika = Player('58cc88171d41c8259fa1fc20', 'Palika')
-Peacecraft = Player('587c636d1d41c82e80d55aaf', 'M. Peacecraft')
-PJ = Player('542cf4d08ab65f5e0c6e0751', 'PJ')
-Poonpounder = Player('5e2fcf55421aa93e12ba4946', 'Poonpounder')
-Project = Player('58cc748e1d41c8259fa1fbb8', 'Project')
-PSai = Player('5d82976f421aa954d874af04', 'PSai')
-Ricky = Player('58cc88171d41c8259fa1fc23', 'Ricky')
-RyuCloud = Player('58c739fa1d41c8259fa1f97f', 'RyuCloud')
-Scooby = Player('59ed70bd1d41c84361b79427', 'Scooby')
-Serb = Player('58cc6fc61d41c8259fa1fb5d', 'Serb')
-Ses = Player('58c72fd21d41c8259fa1f8cc', 'Ses')
-Shmeeli = Player('58811d861d41c82e80d55b56', 'Shmeeli')
-Silver = Player('58c7303b1d41c8259fa1f8dc', 'Silver')
-Slox = Player('542ded228ab65f7d9c8294d1', 'Slox')
-Spiff = Player('58c7321f1d41c8259fa1f8fe', 'Spiff')
-StacysStepdad = Player('5a4ba1961d41c84881946783', 'Stacy\'s Stepdad')
-Stus = Player('58c724e61d41c8259fa1f85c', 'Stus')
-Swoosh = Player('5d6fe5d9421aa954d874adff', 'Swoosh$')
-Takyon = Player('58c73fce1d41c8259fa1fa3c', 'Takyon')
-TedGreene = Player('58b6552e1d41c867e937f7e7', 'Ted Greene')
-Thumbs = Player('58d082921d41c8259fa1fe0a', 'Thumbs')
-Tian = Player('545b23eb8ab65f7a95f74924', 'Tian')
-TimTheGuy = Player('58d333ed1d41c8259fa2001f', 'TimTheGuy')
-Trail = Player('5a93cebe1d41c80e2802a7fa', 'Trail')
-TS420 = Player('59eed00e1d41c84361b795c8', 'TS420')
-Twentytwok = Player('58d0968e1d41c8259fa1fe60', '22K')
-Twisty = Player('58c729941d41c8259fa1f89b', 'Twisty')
-Uma = Player('5a4ba42d1d41c8488194678f', 'Uma')
-Warmmer = Player('58f8246c1d41c850b8b0fa54', 'Warmmer')
-Woodcutting = Player('5b75ec7e421aa91fa088b1ef', '99 Woodcutting')
-Yasu = Player('5a4ba0ba1d41c8488194677a', 'Yasu')
-Younger = Player('54af8e44d2994e1346cd31da', 'Younger')
-
-#store all players into a list
-players = [Slox, Joyboy, Kalvar, Lint, Palika, Thumbs, BigJoig,
-           Warmmer, DrLobster, Clutch, Ses, Peacecraft, Project, GWM420,
-           Bonfire10, Glock, Golden, CNIU, Guillotine, Ben, TimTheGuy, Kikoho, 
-           DrewNG, Arty, Ricky, JNaut, Artelind, Meep, RyuCloud, Guex, Trail, 
-           Silver, Dimension, Yasu, MrHeat, Younger, Klaps, Dudutsai, Tian, PJ, 
-           Hysteric, Spiff, AdmiralZhao, Woodcutting, Shmeeli, PSai, Glasper, 
-           Feo, Swoosh, Omar, Ferox, Hexjo, TedGreene, Alt, BonkCushy, Scooby, 
-           Twisty, Elen, Beansoup, Een, Uma, Twentytwok, Nugget, Serb, 
-           Poonpounder, Giraffe, Stus, IOA, BeastBoy, FutureShock, Kota, 
-           Takyon, Bank, BigRedAnimeBike, Coach, Arcade, TS420, StacysStepdad, 
-           Louis, Lochist]
+players = settings.players
+seasonstart = settings.seasonstart
+seasonend = settings.seasonend
+maxretries = settings.maxretries
 
 #initialize number of retries to access the csv file
 numretries = 0
@@ -226,10 +122,10 @@ for player in players:
                             played = True
              
             #handle additional set Slox and DrLobster played OOR               
-            if(player == Slox and opponent == DrLobster):
+            if(player == settings.Slox and opponent == settings.DrLobster):
                 wins += 1
                 played = True
-            elif(player == DrLobster and opponent == Slox):
+            elif(player == settings.DrLobster and opponent == settings.Slox):
                 losses += 1
                 played = True    
                 
